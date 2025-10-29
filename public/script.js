0099a98341691965476edaafcaddec37d03696db
Akhil <akhiakmtr@gmail.com>
2025-10-29 18:05:37 +0530

    Add main JavaScript logic and Firebase config

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 // 1. Firebase Configuration (REPLACE WITH YOUR OWN FROM STEP 1.3)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY", // <--- REPLACE THIS
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com", // <--- REPLACE THIS
-    projectId: "YOUR_PROJECT_ID", // <--- REPLACE THIS
-    storageBucket: "YOUR_PROJECT_ID.appspot.com", // <--- REPLACE THIS
-    messagingSenderId: "YOUR_SENDER_ID", // <--- REPLACE THIS
-    appId: "YOUR_APP_ID" // <--- REPLACE THIS
+  apiKey: "AIzaSyBPxI_-5oyVIECblHtfEBlc6EfKvS1C5JE",
+  authDomain: "safe-locker-f0df1.firebaseapp.com",
+  projectId: "safe-locker-f0df1",
+  storageBucket: "safe-locker-f0df1.firebasestorage.app",
+  messagingSenderId: "338082864321",
+  appId: "1:338082864321:web:95d9bf7faff05e48194840"
 };
 
 // Initialize Firebase
